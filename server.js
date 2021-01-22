3e4ecc52acf8bdfc9bc114827aeee08c6c1a5601
SirriCelles <sirricelles@gmail.com>
2021-01-22 17:40:37 +0100

    deployment through cli

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/npc-app'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/npc-app/index.html'));});
+
+app.use(express.static(__dirname + '/dist'));
+
 app.listen(process.env.PORT || 8080);
+
+// pathLoaction Strategy
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+console.log('Console Listening');
+// app.use(express.static(__dirname + '/dist/npc-app'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+
+// '/dist/npc-app/index.html'));});
+// app.listen(process.env.PORT || 8080);
